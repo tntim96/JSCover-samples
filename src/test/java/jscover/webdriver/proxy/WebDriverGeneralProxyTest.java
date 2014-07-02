c9421b835be43c1dc7f5dcef44ab4990189f3436
tntim96 <tntim96@gmail.com>
2014-07-02 22:29:03 +1000

    Update to JSCover 1.0.12, JSCover-maven-plugin 1.0.6

--- src/test/java/jscover/webdriver/proxy/WebDriverGeneralProxyTest.java
+++ src/test/java/jscover/webdriver/proxy/WebDriverGeneralProxyTest.java
@@ -45,11 +45,7 @@ public class WebDriverGeneralProxyTest {
     public static void setUpOnce() throws IOException {
         server = new Thread(new Runnable() {
             public void run() {
-                try {
-                    main.runMain(args);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                main.runMain(args);
             }
         });
         server.start();
